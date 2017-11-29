fcf6bd19b09098f445ceed732fc41fe89cb78754
ozsvar66 <ozsvar@yahoo.com>
2017-11-29 16:02:23 +0100

    Create jquery.spectrum-hu.js
    Localization on hungarian language

---
+++ i18n/jquery.spectrum-hu.js
@@ -0,0 +1,18 @@
+// Spectrum Colorpicker
+// Hungarian (hu) localization (by István Ozsvár, istvan@ozsvar.com)
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["hu"] = {
+        cancelText: "Mégsem",
+        chooseText: "Válassz",
+        clearText: "A színválasztás visszaállítása",
+        noColorSelectedText: "Nincs szín kijelölve",
+        togglePaletteMoreText: "Több",
+        togglePaletteLessText: "Kevesebb"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
